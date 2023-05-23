c39b663dda0a3913c5fcafc7c47d10f73ca28588
Matheus Silva <matheustechinfo@gmail.com>
2023-05-23 18:55:27 -0300

    Organização de pacotes e Primeira Class: Position

---
+++ Chess Project/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Position pos = new Position(12, 12);
+        System.out.println(pos.toString()); 
+
+       
+        }
+    }
+
