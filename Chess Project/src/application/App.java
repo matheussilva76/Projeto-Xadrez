431cf2caaa7451aca309a8048ca4e71a15d3142a
Matheus Silva <matheustechinfo@gmail.com>
2023-05-23 19:16:44 -0300

    Começando a implementar Tabuleiro e Peças

--- Chess Project/src/application/App.java
+++ Chess Project/src/application/App.java
@@ -1,12 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(12, 12);
-        System.out.println(pos.toString()); 
+        Board board = new Board(8, 8);
+        System.out.println(board);
 
        
         }
